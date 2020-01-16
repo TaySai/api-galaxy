2e75ce9999139b9ccee14d2fe9cc537e120edb3c
Tayeb Saidi <tsi@ibanfirst.com>
2020-01-16 18:08:43 +0100

    init commit

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from app import app
+from flask import render_template
+from flask import request
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("home.html")
+
+# Return all the galaxies
+@app.route('/api/v1/galaxy/all', methods=['GET'])
+def api_v1_galaxy_all():
+    return 'all galaxies'
+
+# Return information about one galaxy
+@app.route("/api/v1/galaxy/<val>", methods=['GET'])
+def api_v1_galaxy_endpoint(val):
+    data = request.get_json()
+    print(data)
+    return val
+
+# # Return information about multiple galaxies
+# @app.route("/api/v1/galaxy/<val>", methods=['GET'])
+# def api_v1_galaxy_endpoint(val):
+#     print('hello')
+#     return val
+
