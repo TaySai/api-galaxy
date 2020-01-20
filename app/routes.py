9ff51a3ebf20473a6f7b5dbe2cdad48f5dbdeb8c
Saidi Tayeb <saidi.tayeb@hotmail.com>
2020-01-21 00:39:34 +0100

    add scraper

--- app/routes.py
+++ app/routes.py
@@ -14,15 +14,17 @@ def api_v1_galaxy_all():
     return 'all galaxies'
 
 # Return information about one galaxy
-@app.route("/api/v1/galaxy/<val>", methods=['GET'])
+@app.route("/api/v1/galaxy/endpoint/<val>", methods=['GET'])
 def api_v1_galaxy_endpoint(val):
-    data = request.get_json()
-    print(data)
+    print('hello')
     return val
 
-# # Return information about multiple galaxies
-# @app.route("/api/v1/galaxy/<val>", methods=['GET'])
-# def api_v1_galaxy_endpoint(val):
-#     print('hello')
-#     return val
 
+# Return information about one galaxy
+@app.route("/api/v1/galaxies/batch/", methods=['GET'])
+def api_v1_galaxy_batch():
+    data = request.get_json()
+    print(data)
+    if data is None:
+        return 'Error, expected data'
+    return data
---
+++ scrapper/csv_form.py
@@ -0,0 +1,38 @@
+import requests as rq
+from bs4 import BeautifulSoup as bs
+import csv
+
+def create_csv_data():
+    r = rq.get('http://messier.obspm.fr/CONindex2_f.html')
+
+    soup = bs(r.content, 'html.parser')
+
+    data = []
+
+    for x in soup.ul.find_all('li', recursive=False):
+        row = dict()
+        row['galaxy_name'] = x.i.a.get_text().strip()
+        row['galaxy_link'] = 'http://messier.obspm.fr/' + x.i.a['href']
+
+        for y in x.ul.find_all('li'):
+            messier = dict()
+            try:
+                surname = ' '.join(y.b.get_text().split())
+            except AttributeError:
+                surname = ''
+
+            messier['messier_name'] = y.a.get_text().strip()
+            messier['messier_link'] = 'http://messier.obspm.fr/' + y.a['href']
+            messier['messier_surname'] = surname.replace(',', '')
+            messier['messier_form'] = ' '.join(list(filter(None, [x.strip() for x in y(recursive=False, text=True)])))
+
+            messier.update(row)
+
+            data.append(messier)
+
+    with open("data.csv","w",newline="") as file_writer:
+        fields=['messier_name','messier_link','messier_surname','messier_form','galaxy_name','galaxy_link']
+        writer=csv.DictWriter(file_writer,fieldnames=fields)
+        writer.writeheader()
+        for x in data:
+            writer.writerow(x)
